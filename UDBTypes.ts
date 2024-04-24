0b73d00de52bcf52bcbbf9d2dc5de0e019d21293
zzengan <zzengan@connect.ust.hk>
2024-04-24 19:11:04 +0800

    fromjson and tojson for UDB

--- UDBTypes.ts
+++ UDBTypes.ts
@@ -14,4 +14,31 @@ export class UDBInfo {
         this.data = data;
         this.parsed = false;
     }
+    static fromJSON(json: any): UDBInfo {
+        const udbInfo = new UDBInfo(json.data);
+        udbInfo.parsed = json.parsed;
+
+        if (json.classes && Array.isArray(json.classes)) {
+            udbInfo.classes = json.classes.map((classJson: any) =>
+                ClassInfo.fromJSON(classJson)
+            );
+        }
+
+        if (json.functions && Array.isArray(json.functions)) {
+            udbInfo.functions = json.functions.map((funcJson: any) =>
+                FuncInfo.fromJSON(funcJson)
+            );
+        }
+
+        return udbInfo;
+    }
+
+    toJSON(): any {
+        return {
+            data: this.data,
+            parsed: this.parsed,
+            classes: this.classes.map((classInfo) => classInfo.toJSON()),
+            functions: this.functions.map((funcInfo) => funcInfo.toJSON()),
+        };
+    }
 }
